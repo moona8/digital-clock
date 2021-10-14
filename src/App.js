e51d57de31022172828f4351134ee0a82f73b138
moona8 <73126379+moona8@users.noreply.github.com>
2021-10-14 20:25:38 +0500

    deploy

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(){
+    super()
+    this.state={
+      time:new Date(),
+      logginStatus:false
+    }
+  }
+  start () {
+    this.setState({logginStatus:false})
+    
+  };
+  stop () {
+    this.setState({logginStatus:true})
+    
+  };
+  
+    currentTime(){
+      setInterval(()=>{
+        this.setState(this.state.time=new Date())
+      }, 1000)
+    }
+  
+  render(){
+    return(
+      <h1>
+         <button onClick={this.state.logginStatus ? this.start() : this.stop()}>
+            {this.state.logginStatus === true ? "start" : "stop"}
+          </button>
+
+        {this.state.time.toLocaleTimeString()}
+        {this.currentTime()}
+        {/* <button>start</button> */}
+      </h1>
+    )
+ 
+  }
 }
 
 export default App;
